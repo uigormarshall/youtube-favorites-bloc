aa62be531d741231a61e1d06fdd5911d04ffc171
uigormarshall <uigor.silva@acad.ifma.edu.br>
2020-08-17 15:01:56 -0300

    Fix home with MyApp widget steteless

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youtube_favorites/blocs/video_bloc.dart';
 import 'package:youtube_favorites/screens/home.dart';
+import 'package:bloc_pattern/bloc_pattern.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider<VideosBloc>(
+      bloc: VideosBloc(),
+      child: MaterialApp(
+        title: 'Youtube Bloc',
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+      ),
+    );
+  }
 }
